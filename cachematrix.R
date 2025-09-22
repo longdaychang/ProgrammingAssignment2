dfde3d68acfa2928e75fa03308d276b04c5a0a7f
longdaychang <longdaychang@hotmail.com>
2025-09-22 16:58:26 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix -----------------------------------------------------------
+## If the matrix changes via set(), the cached inverse is cleared.
 
 makeCacheMatrix <- function(x = matrix()) {
+inv <- NULL
 
+set <- function(y) {
+if (!is.matrix(y)) stop("Input must be a matrix.")
+x <<- y
+inv <<- NULL # new matrix -> old inverse no longer valid
+}
+
+get <- function() x
+setinv <- function(i) inv <<- i
+getinv <- function() inv
+
+list(set = set, get = get,
+setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve ----------------------------------------------------------------
+## Return the cached inverse if available; otherwise compute first then cache and return.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+inv <- x$getinv()
+if (!is.null(inv)) {
+message("getting cached inverse")
+return(inv)
 }
+
+m <- x$get()
+
+if (!is.matrix(m)) stop("Object does not contain a matrix.")
+if (nrow(m) != ncol(m)) stop("Matrix must be square to invert.")
+
+## use tryCatch if matrix is singular/not invertible.
+inv <- tryCatch(
+solve(m, ...),
+error = function(e) stop("Matrix is singular or not invertible: ", e$message)
+)
+
+x$setinv(inv)
+inv
+}
+
